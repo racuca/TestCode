6c83617bc6d66eb197c238a95d22afbd8ad4db2b
Jason, Lee <racuca@gmail.com>
2020-10-27 17:16:21 +0900

    Image to Data URI Converting Update

--- ImageToText/Form1.Designer.cs
+++ ImageToText/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnConvertToImage = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnBrowse
@@ -49,36 +50,49 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.Size = new System.Drawing.Size(89, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "FileName";
+            this.label1.Text = "Png File Name";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(88, 9);
+            this.textBox1.Location = new System.Drawing.Point(117, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(517, 21);
+            this.textBox1.Size = new System.Drawing.Size(488, 21);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(16, 38);
+            this.textBox2.MaxLength = 0;
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(671, 400);
+            this.textBox2.Size = new System.Drawing.Size(671, 370);
             this.textBox2.TabIndex = 2;
+            // 
+            // btnConvertToImage
+            // 
+            this.btnConvertToImage.Location = new System.Drawing.Point(266, 414);
+            this.btnConvertToImage.Name = "btnConvertToImage";
+            this.btnConvertToImage.Size = new System.Drawing.Size(149, 36);
+            this.btnConvertToImage.TabIndex = 0;
+            this.btnConvertToImage.Text = "Convert To Image";
+            this.btnConvertToImage.UseVisualStyleBackColor = true;
+            this.btnConvertToImage.Click += new System.EventHandler(this.btnConvertToImage_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.ClientSize = new System.Drawing.Size(699, 455);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvertToImage);
             this.Controls.Add(this.btnBrowse);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Image <=> Data URI Text v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +104,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnConvertToImage;
     }
 }
 
--- ImageToText/Form1.cs
+++ ImageToText/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace ImageToText
@@ -17,21 +19,59 @@ namespace ImageToText
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Image <=> Data URI Text v " + this.ProductVersion;
+        }
+
         private void btnBrowse_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "png file|*.png|Image Files|*.*";
+            ofd.Filter = "png file|*.png";
             DialogResult r = ofd.ShowDialog();
             if (r != DialogResult.OK)
             {
                 return;
             }
             string filename = ofd.FileName;
+            textBox1.Text = filename;
 
             byte[] data = File.ReadAllBytes(filename);
             string text = Convert.ToBase64String(data);
 
             textBox2.Text = text;
         }
+
+        private void btnConvertToImage_Click(object sender, EventArgs e)
+        {
+            string data = @"data:image/png;base64," + textBox2.Text;
+
+            var base64Data = Regex.Match(data, @"data:image/(?<type>.+?),(?<data>.+)").Groups["data"].Value;
+            var binData = Convert.FromBase64String(base64Data);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.AddExtension = true;
+            sfd.DefaultExt = ".png";
+            DialogResult r = sfd.ShowDialog();
+            if (r != DialogResult.OK)
+                return;
+
+            string filename = sfd.FileName;
+
+            try
+            {
+                using (var stream = new MemoryStream(binData))
+                {
+                    Bitmap Image = new Bitmap(stream);
+                    Image.Save(filename, ImageFormat.Png);
+                    MessageBox.Show("정상적으로 저장되었습니다.");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("오류가 발생하였습니다. ");
+            }
+        }
+
     }
 }
